7a934d47c3764b429769b841152a22076f3e4175
riley <riley211375@gmail.com>
2024-06-09 14:23:28 +0800

    10：补全 category 数据

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,31 +1,87 @@
 <template>
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
+      ref="ulTarget"
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
       <!-- 汉堡按钮 -->
       <li
+        @click="onShowPopup = !onShowPopup"
         class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
       >
         <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
       </li>
       <!-- category item -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{
+          'text-zinc-100 ': currentCategoryIndex === index
+        }"
       >
         {{ item.name }}
       </li>
     </ul>
+    <m-popup v-model="isVisible">
+      <div>测试内容</div>
+    </m-popup>
   </div>
 </template>
 
 <script setup>
+import { ref, watch, onBeforeUpdate } from 'vue'
+import { useScroll } from '@vueuse/core'
+
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '60px'
+})
+// 选中的 item 下标
+const currentCategoryIndex = ref(0)
+// 获取填充的所有 item 元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// 获取 ul 元素，以计算偏移位置
+const ulTarget = ref(null)
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, (val) => {
+  // 获取选中元素的 left、width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  // 为 sliderStyle 设置属性
+  sliderStyle.value = {
+    // ul 横向滚动位置 + 当前元素的 left 偏移量
+    transform: `translateX(${ulScrollLeft.value + left - 10 + 'px'})`,
+    width: width + 'px'
+  }
+})
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
+// 控制popup展示
+const isVisible = ref(false)
 </script>
