f1927ce7698c81d7fb33cae54c5486c8c9a4325f
riley <riley211375@gmail.com>
2024-06-11 21:31:28 +0800

    08：通用组件：完善 search 基本能力

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,20 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
     extend: {
+      height: {
+        header: '72px',
+        main: 'calc(100vh - 72px)'
+      },
       fontSize: {
         xs: ['0.25rem', '0.35rem'],
         sm: ['0.35rem', '0.45rem'],
         base: ['0.45rem', '0.55rem'],
         lg: ['0.55rem', '0.65rem'],
         xl: ['0.65rem', '0.75rem']
+      },
+      colors: {
+        main: '#f44c58',
+        'hover-main': '#f32836'
       },
       boxShadow: {
         'l-white': '-10px 0 10px white'
