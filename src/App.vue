8c5b412d6f469afa07b3ade37f17bd823307d295
riley <riley211375@gmail.com>
2024-06-16 19:57:13 +0800

    10：通用组件-瀑布流：总结

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- 一级路由出口 -->
-  <router-view></router-view>
+  <div class="h-screen fixed top-0 left-0 w-screen">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup></script>
